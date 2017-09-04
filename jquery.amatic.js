948a54658faaba9666b8cee72af827e863690c36
Renan Diaz <reandimo2@hotmail.com>
2017-09-04 07:35:08 -0400

    Update jquery.amatic.js

--- jquery.amatic.js
+++ jquery.amatic.js
@@ -2,8 +2,8 @@
 // * Amatic JS
 // * Version - 0.1
 // * Licensed under the MIT license - http://opensource.org/licenses/MIT
-// *
-// * Copyright (c) 2017 Renan Diaz
+// * Renan Diaz
+// * Copyright (c) 2017 
 
 
 (function ( $ ) {
